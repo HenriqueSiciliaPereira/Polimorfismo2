2afbe09b25799110fa4e770210f24cc9b7894a47
Henrique Sicilia <siciliahenrique@gmail.com>
2022-09-24 09:33:57 -0300

    Create ProgramaPrincipal.java

---
+++ ProgramaPrincipal.java
@@ -0,0 +1,14 @@
+package polimorfismo2;
+
+public class ProgramaPrincipal {
+
+	public static void main(String[] args) {
+		
+		Cachorro c = new Cachorro();
+		
+		c.reagir(2, 12.5f);
+		c.reagir(17, 4.5f);
+
+	}
+
+}
